121142be09d9530fa11e39d3a5f24986615beada
Tri <tringuyenntt1505@gmail.com>
2023-11-02 21:23:42 +0700

    Add

--- arr.cpp
+++ arr.cpp
@@ -5,24 +5,27 @@ void InputArr(long long arr[], int size);
 void OutputArr(long long arr[], int size);
 
 int main(){
-	int n;
-	cin >> n;
-	long long a[n];
-	InputArr(a, n);
-	cin >> n;
-	long long b[n];
-	InputArr(b, n);
+	int n1, n2;
+	cin >> n1;
+	long long a[n1];
+	InputArr(a, n1);
+	cin >> n2;
+	long long b[n2];
+	InputArr(b, n2);
 
-	long long c[n];
-	for (int i = 0; i < n; i++){
-		if (i <= n/2){
+
+	int n3 = (n1 > n2) ? n1: n2;
+	long long c[n3];
+	for (int i = 0; i < n3; i++){
+		if (i < n3/2+1){
 			c[i] = a[i];
 		}else{
 			c[i] = b[i];
 		}
 	}
-	OutputArr(c, n);
+	OutputArr(c, n3);
 
+	return 0;
 }
 
 void InputArr(long long arr[], int size){
